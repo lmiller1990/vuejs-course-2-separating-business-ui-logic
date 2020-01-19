87bf4f0f38b4d2f190d0abf309329d99e252dd37
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-01-19 17:30:37 +1000

    finish refactor

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,8 +1,38 @@
 import { shallowMount } from '@vue/test-utils'
 import SimplePassword from '@/SimplePassword.vue'
+import { checkPassword } from '@/logic'
+
+const risky = 'asdfasfd'
+const secure = 'asdfsASDFASD1231@?@'
 
 describe('SimplePassword.vue', () => {
   it('renders', () => {
-    const wrapper = shallowMount(SimplePassword)
+    const wrapper = shallowMount(SimplePassword, {
+      propsData: {
+        password: risky
+      }
+    })
+
+    expect(wrapper.classes()).toContain('risky')
+  })
+
+  it('renders', () => {
+    const wrapper = shallowMount(SimplePassword, {
+      propsData: {
+        password: secure
+      }
+    })
+
+    expect(wrapper.classes()).toContain('secure')
+  })
+})
+
+describe('checkPassword', () => {
+  it('return risky', () => {
+    expect(checkPassword(risky)).toBe('risky')
+  })
+
+  it('return secure', () => {
+    expect(checkPassword(secure)).toBe('secure')
   })
 })
